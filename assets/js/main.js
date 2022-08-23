0081ec889c18e9bf03c806ac5bf0c60720ef1831
Denis <like-nix@mail.ru>
2022-08-23 18:14:37 +0300

    dark mode example

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
-import '../css/style.css'
+import "../css/style.css";
 
+window.onload = () => {
+    document.querySelector(".ButtonSwitch").addEventListener("click", () => {
+        if (document.documentElement.hasAttribute("class")) {
+            document.documentElement.removeAttribute("class");
+        } else {
+            document.documentElement.classList.add("dark");
+        }
+    });
+};
