7121204a863b282e700ff935378e0c32de95af69
Denis <like-nix@mail.ru>
2022-08-28 00:30:08 +0300

    temp commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,15 @@
 import '../css/style.css'
 
+const $box = document.querySelector(".box")
+$box.addEventListener("click", (e) => {
+    const $item = e.target.closest(".item")
+    if (!$item) return
+
+    for (let i = 0; i < $box.children.length; i++) {
+        if ($box.children[i] === $item) {
+            $box.children[i].classList.add("active")
+        } else {
+            $box.children[i].classList.remove("active")
+        }
+    }
+})
